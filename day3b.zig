961d928871e810c972ae0a515b4fd8b25ace54e9
Elliot Bulmer <egbulmer@icloud.com>
2024-12-09 12:48:07 +0800

    Day 3 - B

---
+++ day3b.zig
@@ -0,0 +1,67 @@
+const std = @import("std");
+const input = @embedFile("day3_input.txt");
+
+const do = "do()";
+const dont = "don't()";
+
+pub fn main() void {
+    // Length of "mul(x,y)", the shortest valid multiplication.
+    const min_size = 8;
+    // Length of "mul(xxx,yyy)" the longest valid multiplication.
+    const max_size = min_size + 4;
+
+    var enabled = true;
+    var result: i32 = 0;
+
+    var win_it = std.mem.window(u8, input, max_size, 1);
+    while (win_it.next()) |s| {
+        // Check string is long enough for shortest op = "do()".
+        if (s.len < do.len) continue;
+
+        // Handle the "do()" and "don't()" commands before trying to parse a "mul()".
+        if (std.mem.eql(u8, s[0..do.len], do)) {
+            enabled = true;
+            continue;
+        }
+
+        if (std.mem.eql(u8, s[0..dont.len], dont)) {
+            enabled = false;
+            continue;
+        }
+
+        // String must be long enough for valid operation.
+        if (s.len < min_size) continue;
+        // String must start with 'mul('.
+        if (!std.mem.eql(u8, s[0..4], "mul(")) continue;
+
+        var start: usize = 4;
+        var end = start + 1;
+
+        // Advance while looking at a digit character or until we hit the end.
+        while (end < s.len and isDigit(s[end])) end += 1;
+        if (end >= s.len) continue;
+
+        const x = std.fmt.parseInt(i32, s[start..end], 10) catch continue;
+
+        // End should be pointing at ',' immediately before the second parameter.
+        start = end + 1;
+        end = start + 1;
+
+        // Advance while looking at digit character or until we hit the end. When we are done
+        // check that end is looking at a ')'.
+        while (end < s.len and isDigit(s[end])) end += 1;
+        if (end >= s.len or s[end] != ')') continue;
+
+        const y = std.fmt.parseInt(i32, s[start..end], 10) catch continue;
+
+        if (enabled) {
+            result += x * y;
+        }
+    }
+
+    std.debug.print("Answer = {d}\n", .{result});
+}
+
+fn isDigit(ch: u8) bool {
+    return ch >= '0' and ch <= '9';
+}
